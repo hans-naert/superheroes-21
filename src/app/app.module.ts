4fe5867788b9698c4f66d0e67e250989071311bf
VIVES-ELICT <hans.naert@vives.be>
2021-09-25 19:28:12 +0200

    getting started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// Import toolbar and card modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
+
+/* Considering it is a getting started sample, importing noop animation
+module, disabling animations. */
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
