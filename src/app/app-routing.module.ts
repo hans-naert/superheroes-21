f0563419ead7f0abc8b10ba81479abdd25abcbfd
hans-naert <hans.naert@vives.be>
2021-11-14 21:44:47 +0100

    add create-hero route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateSuperheroReactiveFormComponent } from './superheroes-material-design/create-superhero-reactive-form/create-superhero-reactive-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ /* {
+    path: "heroes",
+    component: SuperheroListComponent
+  },*/
+  {
+    path: "create-hero",
+    component: CreateSuperheroReactiveFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
