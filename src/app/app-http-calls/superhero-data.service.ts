31996cc6bafb06115857cca5071c62da438bfb1d
Hans Naert <hans.naert@vives.be>
2021-12-12 20:07:58 +0100

    HttpClient with explicitly typed response

--- src/app/app-http-calls/superhero-data.service.ts
+++ src/app/app-http-calls/superhero-data.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Superhero } from '../superheroes-material-design/models/superhero';
 
 const URL_PREFIX = "http://localhost:3000";
 
@@ -10,10 +11,10 @@ const URL_PREFIX = "http://localhost:3000";
 export class SuperheroDataService {
   constructor(private httpClient: HttpClient) { }
   getHeroes() {
-    let heroes: Observable<any> = this.httpClient.get(`${URL_PREFIX}/heroes`);
+    let heroes: Observable<Array<Superhero>> = this.httpClient.get<Array<Superhero>>(`${URL_PREFIX}/heroes`);
     heroes.subscribe(
       {next :(data) => {
-        console.log(data.map((hero: any) => ({
+        console.log(data.map((hero) => ({
           "name": hero.name || "",
           "email": hero.email || "",
           "details": hero.details || "",
